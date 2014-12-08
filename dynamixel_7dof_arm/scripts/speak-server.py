6b79eb415e8262498fc4e9ab7121af8a41eb9264
mechuser <mechuser@IMI-T400s.(none)>
2014-12-08 13:35:58 +0900

    change name

--- dynamixel_7dof_arm/scripts/speak-server.py
+++ dynamixel_7dof_arm/scripts/speak-server.py
@@ -1,20 +1,46 @@
-#!/usr/bin/env python
-# license removed for brevity
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+import urllib, pycurl, os
+
 import rospy
 from std_msgs.msg import String
-import time
 
-def talker():
-    pub = rospy.Publisher('speaker', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    r = rospy.Rate(1) # 10hz
-    while not rospy.is_shutdown():
-        str = "I am turtlebot"
-        rospy.loginfo(str)
-        pub.publish(str)
-        time.sleep(5)
+
+def callback(data):
+    rospy.loginfo("speak {}".format(data.data))
+    speakSpeechFromText(data.data)
+    # rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
+
+def downloadFile(url, fileName):
+    fp = open(fileName, "wb")
+    curl = pycurl.Curl()
+    curl.setopt(pycurl.URL, url)
+    curl.setopt(pycurl.WRITEDATA, fp)
+    curl.perform()
+    curl.close()
+    fp.close()
+
+def getGoogleSpeechURL(phrase, select_english=True):
+    if select_english:
+        googleTranslateURL = "http://translate.google.com/translate_tts?tl=en&"
+    else:
+        googleTranslateURL = "http://translate.google.com/translate_tts?tl=ja&"
+    parameters = {'q': phrase}
+    data = urllib.urlencode(parameters)
+    googleTranslateURL = "%s%s" % (googleTranslateURL,data)
+    return googleTranslateURL
+
+def speakSpeechFromText(phrase):
+    googleSpeechURL = getGoogleSpeechURL(phrase, select_english=True)
+    downloadFile(googleSpeechURL,"tts.mp3")
+    os.system("mplayer tts.mp3 &")
+
+from sound_play.msg import *
+
+def main():
+    rospy.init_node('robotspeaker', anonymous=True)
+    rospy.Subscriber("speaker", String, callback)
+    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException: pass
+    main()
