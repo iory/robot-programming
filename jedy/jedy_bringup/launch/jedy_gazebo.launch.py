5ab0402afee1e37d0c26ee2e85ebf8aae2130e66
Iori Yanokura <ab.ioryz@gmail.com>
2025-09-24 18:04:13 +0900

    Add

--- jedy/jedy_bringup/launch/jedy_gazebo.launch.py
+++ jedy/jedy_bringup/launch/jedy_gazebo.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     gui = LaunchConfiguration('gui', default='true')
     headless = LaunchConfiguration('headless', default='false')
-    world_file = LaunchConfiguration('world_file', default=os.path.join(get_package_share_directory('jedy_bringup'), 'worlds', 'simple.world'))
+    
     model_file = LaunchConfiguration('model', default=os.path.join(get_package_share_directory('jedy_description'), 'urdf', 'jedy_no_arm.urdf'))
     rviz_config = LaunchConfiguration('rviz_config', default=os.path.join(get_package_share_directory('jedy_bringup'), 'config', 'jedy.rviz'))
     controllers_config = LaunchConfiguration('controllers_config', default=os.path.join(get_package_share_directory('jedy_bringup'), 'config', 'jedy_controllers.ros2.yaml'))
@@ -33,7 +33,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-        launch_arguments={'gz_args': [' -r -v 4 ', world_file], 'on_exit_shutdown': 'true'}.items(),
+        launch_arguments={'on_exit_shutdown': 'true'}.items(),
     )
 
     # Robot description
@@ -100,7 +100,7 @@ def generate_launch_description():
         DeclareLaunchArgument('use_sim_time', default_value='true'),
         DeclareLaunchArgument('gui', default_value='true'),
         DeclareLaunchArgument('headless', default_value='false'),
-        DeclareLaunchArgument('world_file', default_value=os.path.join(get_package_share_directory('jedy_bringup'), 'worlds', 'empty.world')),
+        
         DeclareLaunchArgument('model', default_value=os.path.join(get_package_share_directory('jedy_description'), 'urdf', 'jedy_no_arm.urdf')),
         DeclareLaunchArgument('rviz_config', default_value=os.path.join(get_package_share_directory('jedy_bringup'), 'config', 'jedy.rviz')),
         DeclareLaunchArgument('controllers_config', default_value=os.path.join(get_package_share_directory('jedy_bringup'), 'config', 'jedy_controllers.ros2.yaml')),
