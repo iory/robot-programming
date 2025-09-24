19f0f0af37f0efa70b8bfdae3149deb2503a14a4
Iori Yanokura <ab.ioryz@gmail.com>
2025-09-24 21:49:21 +0900

    Add

--- jedy/jedy_bringup/launch/jedy_gazebo_working.launch.py
+++ jedy/jedy_bringup/launch/jedy_gazebo_working.launch.py
@@ -106,7 +106,7 @@ def generate_launch_description():
     return LaunchDescription([
         SetEnvironmentVariable(name='LIBGL_ALWAYS_SOFTWARE', value='1'),
         SetEnvironmentVariable(name='OGRE_RTT_MODE', value='Copy'),
-        SetEnvironmentVariable(name='GAZEBO_RESOURCE_PATH', value=os.path.join(pkg_jedy_description, 'meshes')),
+        SetEnvironmentVariable(name='GZ_SIM_RESOURCE_PATH', value=pkg_jedy_description),
         
         gazebo,
         robot_state_publisher,
