99e7cd0babfb415cb16ce89b055f25432d6ebc18
Iori Yanokura <ab.ioryz@gmail.com>
2025-11-10 13:34:52 +0900

    Add

--- jedy/jedy_bringup/launch/jedy_gazebo.launch.py
+++ jedy/jedy_bringup/launch/jedy_gazebo.launch.py
@@ -314,6 +314,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         SetEnvironmentVariable(name='GZ_SIM_RESOURCE_PATH', value=gz_resource_path),
+        SetEnvironmentVariable(name='GZ_FILE_PATH', value=model_path),
         SetEnvironmentVariable(name='DISPLAY', value=':1'),
         DeclareLaunchArgument('use_sim_time', default_value='true'),
         gazebo,
